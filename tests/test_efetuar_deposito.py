4854605c414589d5294420ee6d89d07587447a0d
Alisson Deivison <uepbalisson@gmail.com>
2025-11-07 19:45:19 -0300

    Teste: adicionar testes de depósito com valores negativos e zero
    Adicionar casos de teste para verificar o tratamento adequado de erros ao tentar depositar valores zero ou negativos em uma conta bancária.

--- tests/test_efetuar_deposito.py
+++ tests/test_efetuar_deposito.py
@@ -5,3 +5,13 @@ def test_efetuar_deposito():
     conta = ContaBancaria(12345, "Alisson Deivison", 0)
     conta.depositar(1000.0)
     assert conta.saldo == 1000.0
+
+def test_efetuar_deposito_zero():
+    conta = ContaBancaria(12345, "Alisson Deivison", 0)
+    with pytest.raises(ValueError, match="Valor de depósito deve ser maior que zero"):
+        conta.depositar(0.0)
+        
+def test_efetuar_deposito_valor_negativo():
+    conta = ContaBancaria(12345, "Alisson Deivison", 0)
+    with pytest.raises(ValueError, match="Valor de depósito deve ser maior que zero"):
+        conta.depositar(-1000.0)
